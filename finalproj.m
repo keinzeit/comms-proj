66f356da8b7b127b7c720b6df1861597458ccda5
Kenny <kenny.yau@tufts.edu>
2017-11-29 21:18:01 -0500

    deleted old comments

--- finalproj.m
+++ finalproj.m
@@ -15,64 +15,7 @@
 %     - Converion to Image
 %     - Image post-processing
 
-% Stuff to do:
-
-%% Image pre-process:
-% Find own image
-%{
-filename = 'heartstringtug.jpeg';
-
-I = imread(filename);
-I = imrotate(I,270,'bilinear');
-figure(1); imshow(I); title('Original Image')
-Z = rgb2gray(I); % convert to grayscale
-figure(2); imshow(Z); title('Grayscaled Image')
-%}
-
-% convert image to double data type using im2double
-% make sure image is m-by-n, where both m and n are divisible by 8
-% perform DCT image in 8x8 blocks using blkproc
-% scale all values to fit between 0 and 1
-% quantize the scaled, transformed image using code from hw 7
-% use reshape and permute to convert the scaled image? into what?
-%     - reshape reorganizes a matrix with certain dimensions and size into a matrix of another dimension and size
-%     - we are organizing the image data into 8x8 blocks by stacking them on top of each other
-%     - use permute to make sure first two dimensions specify a single 8x8 block
-% 
-%% Conversion to bit-stream:
-% we transmit N DCT blocks at a time
-% Convert N DCT blocks into a single column vector
-% convert each element in this column vector into an 8-bit binary number using de2bi, stored as a row
-%     - ex [5] -> [1 0 1 0 0 0 0 0]
-
-% ex start with N=2 blocks of 8x8 DCT blocks
-
-% rows = 8; cols = 8;
-% dct1 = randi(20,rows,cols)
-% dct2 = randi(20,rows,cols)
-% dct(:,:,1) = dct1;
-% dct(:,:,2) = dct2;
-
-% Assumptions: dct is a bunch of 8x8 dct blocks stacked on top of each other
-% and that the values in dct are quantized to integer values
-
-% get dimensions of dct
-% [M,N,B] = size(dct);
-% 
-% len = M*N;
-% bs = zeros(M*N*B,1); %preallocate memory for vector
-% for i = 1:B
-%     bs_temp = reshape(dct(:,:,i),len,1); % reshape B MxN dct blocks into a long column vector
-%     start = len*(i-1)+1
-%     stop  = len*i
-%     bs(start:stop) = bs_temp; % put values into correct location in bs
-% end
-% 
-% bs = de2bi(bs,8); % convert quantized values into 8-bit numbers. right MSB; use correct flag for left MSB 
-
-
-
-%% final project
+%% Image Pre-processing
 I = imread('file.jpeg');                % Import Image
 I = imrotate(I, 270);                   % Rotate Image
 I = imresize(I, 0.25);                  % Resize image
@@ -90,8 +33,8 @@ n = size(Z,2);                          % number of columns
 fun = @dct2;
 dctZ= blkproc(Z,[8,8],fun);             %Perform DCT on image
 
-figure(2)
-imshow(dctZ)                            %Display the DCT of image
+%figure(2)
+%imshow(dctZ)                            %Display the DCT of image
 
 % scale the DCT image matrix from 0-1
 scaledZ = (dctZ-min(dctZ(:)))./(max(dctZ(:)-min(dctZ(:))));
@@ -132,7 +75,11 @@ for i = 1:B
     bs(start:stop) = bs_temp;  % put values into correct location in bs
 end
  
-bs = de2bi(bs,qbits); % convert quantized values into 8-bit numbers. right MSB; use correct flag for left MSB
+% bs = de2bi(bs,qbits); % convert quantized values into 8-bit numbers. right MSB; use correct flag for left MSB
+bs = [1 0 1 1 1 0 0 0 0 1 1 0 0 0 1 1
+      0 1 1 1 0 0 0 0 1 1 0 0 0 1 0 0
+      0 1 1 0 0 0 0 0 1 0 0 1 1 0 1 0
+      0 1 1 1 0 1 1 0 0 1 1 1 0 1 0 1];
 
 %% Modulation
 T = 32; % number of samples per pulse
@@ -153,28 +100,18 @@ R = qbits/(qbits/log2(PAM_level)); % number of quantization bits
 
 % calculate amplitudes for all pulses
 Am = zeros(M*N*B,R);
-for i = 1:M*N*B
-    for j = 1:R % perform 16-PAM
-        head = 1+4*(j-1);
-        tail = 4*j;
+for i = 1:4% M*N*B
+    for j = 1:qbits/R % perform 16-PAM
+        head = 1+log2(PAM_level)*(j-1);
+        tail = log2(PAM_level)*j;
         Am(i,j) = bi2de(bs(i,head:tail));
     end
 end
-sp1 = Am(1,1)*sPulse; srrcp1 = Am(1,1)*srrcPulse;
+if PAM_level ==2
+    Am(Am==0) = -1;
+end
+
+sp1 = Am(1,1)*sPulse; srrcp1 = Am(2,1)*srrcPulse;
 plot(t1,sp1,t2,srrcp1)
 
-
-
-
-
-% TEST = zeros(1,64*5+1);
-% TEST(1:257) = TEST(1:257) + Am(1,1)*srrcPulse;
-% TEST(65:321) = TEST(65:321) + Am(1,2)*srrcPulse
-% plot(TEST)
-% hold on
-% plot(1:257,Am(1,1)*srrcPulse)
-% hold on
-% plot(65:321,Am(1,2)*srrcPulse)
-
-
-
+signalSP = zeros(1,size(Am,1)*size(Am,2)*T);
