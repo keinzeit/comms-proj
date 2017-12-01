745e81d8a050a5a5aefb7fbac8340960596f6a1c
Jon Manninen <jonmanninen@gmail.com>
2017-12-01 17:45:50 -0500

    added channel plots

--- qscript.m
+++ qscript.m
@@ -9,7 +9,10 @@ sPulse = sin(pi*t1/T);
 
 figure(101); plot(t11,sPulse)
 title('half-sine pulse'); xlabel('Time (s)')
+
 figure(102); freqz(sPulse)
+
+
 
 % srrc pulse
 A = norm(sPulse,2);
@@ -20,3 +23,31 @@ srrcPulse = A*rcosdesign(a,K,2*T,'sqrt'); % energy of rcosdesign is one, just mu
 
 figure(111); plot(t21,srrcPulse)
 figure(112); freqz(srrcPulse)
+
+figure(113)
+plot(tModBsSP(1:T*10),modBsSP(1:T*10))
+title('10 Random Symbols w/ Half-Sine Pulse')
+xlabel('Time (s)')
+ylabel('Amplitude')
+
+figure(114)
+freqz(tModBsSP)
+
+delay = zeros(1, 31);                           %vector of 31 zeros to space between values in h
+h = [1 delay 1/2 delay 3/4 delay -2/7];         % channel filter FIR
+
+figure(115)
+freqz(h)
+
+figure(116)
+stem(h)
+axis([-10 105 -.5 1.5])
+
+ysine = conv(modBsSP(1:T*10), h);    %channel output 
+figure(117)
+plot(ysine)
+
+
+ysrrc = conv(modStreamSRRC, h);                 %channel output
+
+
