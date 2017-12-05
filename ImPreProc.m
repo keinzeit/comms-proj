0a264081ce14f5c18c39ceeee11146528fa47395
Jon Manninen <jonmanninen@gmail.com>
2017-12-05 18:49:49 -0500

    final project functions

---
+++ ImPreProc.m
@@ -0,0 +1,37 @@
+function [Zt dctZ I] = ImPreProc(I0)
+
+%function imports the image to be used, processes the image by converting
+%to a matrix and scaling the data between 0 and 1 for quantization.  The
+%output of this function is a 3d array of 8x8 binary matrices
+
+% no inputs
+% outputs Zt, a 3d array of 8x8 matrices
+
+
+I = I0(801:864,401:464,:);              % Crop Image
+I = imresize(I, 0.5);                  % Resize image
+
+%figure(1)
+%imshow(I0)                              % Display original image
+
+Z = rgb2gray(I);                        % Convert to grayscale
+Z = im2double(Z);                       % Convert image to type double
+
+m = size(Z,1);                          % Get number of rows
+n = size(Z,2);                          % number of columns
+
+%figure(2)
+%imshow(Z)                               %display the cropped/scaled image
+
+fun = @dct2;
+dctZ= blkproc(Z,[8,8],fun);             %Perform DCT on image
+
+%figure(3)
+%imshow(dctZ)                            %Display the DCT of image
+
+            % scale the DCT image matrix from 0-1
+minZ = min(dctZ(:)); maxZ = max(dctZ(:));
+scaledZ = (dctZ-min(dctZ(:)))./(max(dctZ(:)-min(dctZ(:))));
+
+
+Zt = reshape(scaledZ, [8,8,(m*n)/64]);  % Outputs a 3D array of scaled data
---
+++ ModulatedSRRCPulse.m
@@ -0,0 +1,38 @@
+function [modBsSRRC modStreamSRRC] = ModulatedSRRCPulse(bitStream, alpha, T, K, Am, M, N, B, qBits, PAM_level)
+
+% Function inputs the bitStream and modulates the bit stream using a Square
+% Root Raised Cosine shaping filter.  The function creates the SRRC pulse
+% and then modulates the bit stream
+
+t2 = linspace(-K*T,K*T,2*K*T+1);
+R = log2(PAM_level); % number of bits used to change amplitude of pulse shape
+pulsesPerRow = qBits/R; % number of pulses made from a row of Am
+
+%Create the SRRC pulse
+modBsSRRC = zeros(1, length(t2));
+for i = 1:length(modBsSRRC)
+    if t2(i) == 0
+        %denominator equals 0
+        modBsSRRC(i) = 1 - alpha + 4*alpha/pi;
+    elseif abs(t2(i)) == T/(4*alpha)
+        %denominator equals 0
+        modBsSRRC(i) = (alpha/sqrt(2)) * ((1+(2/pi))*sin(pi/(4*alpha)) + (1-(2/pi))*cos(pi/(4*alpha)));
+    else
+        modBsSRRC(i) = (sin(pi*t2(i)/T*(1-alpha)) + 4*alpha*t2(i)/T*cos(pi*t2(i)/T*(1+alpha))) / (pi*t2(i)/T*(1-((4*alpha*t2(i)/T)^2)));
+    end 
+end
+
+
+
+
+modStreamSRRC = zeros(1,(size(Am,1)*size(Am,2)+2*K-1)*T+1);
+% tModBsSP = (1:length(modStreamSRRC))/T;
+for i = 1:M*N*B*qBits
+    j = fix((i-1)/pulsesPerRow)+1;
+    k = mod(i-1,pulsesPerRow)+1;
+    start = (i-1)*T+1;
+    stop  = (i-1)*T+2*K*T+1;
+    modStreamSRRC(start:stop) = modStreamSRRC(start:stop) + Am(j,k)*modBsSRRC; %consider removing modBsSP for half-sine pulse  
+end
+
+
